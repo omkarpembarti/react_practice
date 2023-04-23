4d8f33a7bebb91031d88805ffe124f895975a4c3
OmkarPembarti <omkarpembarti@gmail.com>
2023-04-24 01:44:16 +0530

    Added ErrorBoundary componet into App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 
 import './App.css';
+import ErrorBoundary from './components/ErrorBoundary';
+
+function Card({ surname }) {
+  if (surname === "pembarti")
+    throw new Error("Error in Custom Card component");
+  return <div>{surname}</div>;
+}
+
 
 function App() {
   return (
     <div className="App">
-      APP
+      <ErrorBoundary>
+        <div>Omkar</div>
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <div>Gopal</div>
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <div><Card surname="sai" /></div>
+      </ErrorBoundary>
     </div>
   );
 }
